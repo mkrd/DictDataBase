6e7f46df152214aac1f83a86c5dd3c598cc88dbb
Marcel Kröker <kroeker.marcel@gmail.com>
2022-01-31 19:31:04 +0100

    IMPROVE Tests

--- test_parallel_runner.py
+++ test_parallel_runner.py
@@ -2,9 +2,13 @@ import dictdatabase as DDB
 from multiprocessing import Pool
 import sys
 
-def incr_db(n, tables, storage_directory):
-	DDB.config.storage_directory = storage_directory
-	DDB.config.pretty_json_files = False
+
+
+def incr_db(n, tables, ddb_sd, ddb_pj, ddb_uc):
+	DDB.config.storage_directory = ddb_sd
+	DDB.config.pretty_json_files = ddb_pj
+	DDB.config.use_compression = ddb_uc
+
 	for _ in range(n):
 		for t in range(tables):
 			with DDB.session(f"incr{t}", as_PathDict=True) as (session, d):
@@ -14,21 +18,19 @@ def incr_db(n, tables, storage_directory):
 
 
 
+if __name__ == "__main__":
+	tables = int(sys.argv[1])
+	processes = int(sys.argv[2])
+	per_process = int(sys.argv[3])
+	ddb_sd = str(sys.argv[4])
+	ddb_pj = bool(int(sys.argv[5]))
+	ddb_uc = bool(int(sys.argv[6]))
 
-def test_stress(tables, processes, per_process, storage_directory):
-	DDB.config.storage_directory = storage_directory
+	print(sys.argv)
+	print(f"{tables = }, {processes = }, {per_process = }, {ddb_sd = }, {ddb_pj = }, {ddb_uc = }")
+
 	pool = Pool(processes=processes)
 	for _ in range(processes):
-		pool.apply_async(incr_db, args=(per_process, tables, storage_directory,))
+		pool.apply_async(incr_db, args=(per_process, tables, ddb_sd, ddb_pj, ddb_uc,))
 	pool.close()
 	pool.join()
-
-
-
-if __name__ == "__main__":
-	test_stress(
-		tables=int(sys.argv[1]),
-		processes=int(sys.argv[2]),
-		per_process=int(sys.argv[3]),
-		storage_directory=sys.argv[4]
-	)
