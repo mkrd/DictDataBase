9fa1bdf39bb8aa1d199da86ecd2abb9f7d8ba336
Marcel Kröker <kroeker.marcel@gmail.com>
2022-10-23 08:47:28 +0200

    add scalene profiler

--- test_profiler.py
+++ test_profiler.py
@@ -2,12 +2,55 @@ import cProfile
 import dictdatabase as DDB
 import subprocess
 import orjson
+from scalene import scalene_profiler
+
 
 DDB.config.storage_directory = "./test_db/production_database"
 
 
-def orjson_decode(data_str):
+
+
+
+
+def orjson_decode(data_str, select_key: str = "fM44"):
+
     return orjson.loads(data_str)
+    # select_start = f'"{select_key}": '
+    # key_index = data_str.find(select_start)
+    # print(key_index)
+
+    # in_str = False
+    # in_lst = 0
+    # in_dct = 0
+    # for i in range(key_index + len(select_start), len(data_str)):
+    #     if data_str[i] == '"' and data_str[i-1] != "\\":
+    #         in_str = not in_str
+    #         continue
+    #     if in_str:
+    #         continue
+    #     if data_str[i] == "[":
+    #         in_lst += 1
+    #     elif data_str[i] == "]":
+    #         in_lst -= 1
+    #     elif data_str[i] == "{":
+    #         in_dct += 1
+    #     elif data_str[i] == "}":
+    #         in_dct -= 1
+    #     if in_lst == 0 and in_dct == 0:
+    #         i += 1
+    #         if data_str[i] == ",":
+    #             i += 1
+    #         break
+
+    # load_str = data_str[key_index + len(select_start):i]
+    # if load_str[-1] == ",":
+    #     load_str = load_str[:-1]
+    # print(load_str)
+    # loaded = orjson.loads(load_str)
+    # return loaded
+
+
+
 
 
 def orjson_encode(data_dict):
@@ -21,17 +64,13 @@ DDB.config.custom_json_encoder = orjson_encode
 DDB.config.custom_json_decoder = orjson_decode
 
 
-with cProfile.Profile() as pr:
-    pr.enable()
+scalene_profiler.start()
 
-    for _ in range(200):
-        with DDB.session("tasks", as_PathDict=True) as (session, tasks):
-            tasks["00000"] = lambda x: (x or 0) + 1
-            session.write()
 
-    pr.disable()
-    pr.dump_stats("test.prof")
-    pr.print_stats("tottime")
+with DDB.session("tasks") as (session, tasks):
+    print("sess", len(tasks))
+    session.write()
+scalene_profiler.stop()
 
-command = "poetry run snakeviz test.prof"
-subprocess.call(command.split())
+# command = "poetry run snakeviz test.prof"
+# subprocess.call(command.split())
