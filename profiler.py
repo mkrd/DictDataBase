9b20d0a035d656dc3924f12225a0a70ce738e59f
Marcel Kröker <kroeker.marcel@gmail.com>
2022-10-26 16:52:28 +0200

    improve profiler

--- profiler.py
+++ profiler.py
@@ -1,5 +1,5 @@
 import dictdatabase as DDB
-import path_dict as pd
+from path_dict import PathDict
 from pyinstrument import profiler
 
 
@@ -9,7 +9,10 @@ DDB.config.use_orjson = True
 
 p = profiler.Profiler(interval=0.00001)
 with p:
-    with DDB.at("tasks").session(key="fM44", as_type=pd) as (session, task):
+    # fM44 is small
+    # a2lU has many annotations
+    # DDB.at("tasks").read(key="fM44", as_type=PathDict)
+    with DDB.at("tasks").session(key="a2lU", as_type=PathDict) as (session, task):
         task["jay"] = lambda x: (x or 0) + 1
         session.write()
-p.open_in_browser()
+p.open_in_browser(timeline=True)
