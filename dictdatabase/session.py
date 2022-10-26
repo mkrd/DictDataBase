2a48b039f99a1f204561ccc41fc8b4a10bb41b3e
Marcel Kröker <kroeker.marcel@gmail.com>
2022-10-26 11:22:29 +0200

    improvements

--- dictdatabase/session.py
+++ dictdatabase/session.py
@@ -7,6 +7,12 @@ T = TypeVar("T")
 JSONSerializable = TypeVar("JSONSerializable", str, int, float, bool, None, list, dict)
 
 
+class SessionType:
+	SINGLE = 0
+	MULTI = 1
+	SUB = 2
+
+
 class DDBSession(Generic[T]):
 	"""
 		Enter:
@@ -23,17 +29,16 @@ class DDBSession(Generic[T]):
 	def __init__(self, db_name: str, key: str = None, as_type: T = None):
 		self.db_name = db_name
 		self.as_type = as_type
-
 		if key is not None:
 			if "*" in key:
 				raise ValueError("A key cannot be specified with a wildcard.")
 			self.key = key
-			self.session_type = "sub"
+			self.session_type = SessionType.SUB
 		elif "*" in db_name:
 			self.db_name = utils.find(db_name)
-			self.session_type = "multi"
+			self.session_type = SessionType.MULTI
 		else:
-			self.session_type = "single"
+			self.session_type = SessionType.SINGLE
 
 	def __enter__(self) -> Tuple["DDBSession", JSONSerializable | T]:
 		"""
@@ -44,55 +49,48 @@ class DDBSession(Generic[T]):
 			No new read tasks will be allowed. When all read tasks are done, the session aquire the write lock.
 			Now, it can savely read and write while all other tasks wait.
 		"""
-		if self.session_type in ("single", "sub"):
+		if self.session_type in (SessionType.SINGLE, SessionType.SUB):
 			self.write_lock = locking.WriteLock(self.db_name)
 		else:
-			self.write_locks = [locking.WriteLock(x) for x in self.db_name]
+			self.write_lock = [locking.WriteLock(x) for x in self.db_name]
 		self.in_session = True
 
 		try:
-			if self.session_type == "single":
-				self.data_handle = io_unsafe.read(self.db_name)
-			elif self.session_type == "sub":
+			if self.session_type == SessionType.SINGLE:
+				dh = io_unsafe.read(self.db_name)
+				self.data_handle = dh
+			elif self.session_type == SessionType.SUB:
 				self.partial_handle = io_unsafe.partial_read(self.db_name, self.key)
-				self.data_handle = self.partial_handle.key_value
-			elif self.session_type == "multi":
+				dh = self.partial_handle.key_value
+				self.data_handle = dh
+			elif self.session_type == SessionType.MULTI:
 				self.data_handle = {n: io_unsafe.read(n) for n in self.db_name}
-
-
-			if self.as_type is not None:
-				return self, self.as_type(self.data_handle)
-			return self, self.data_handle
-
-
+			return self, self.as_type(dh) if self.as_type is not None else dh
 		except BaseException as e:
-			if self.session_type in ("single", "sub"):
-				self.write_lock.unlock()
-			else:
-				for lock in self.write_locks:
+			if self.session_type == SessionType.MULTI:
+				for lock in self.write_lock:
 					lock.unlock()
+			else:
+				self.write_lock.unlock()
 			raise e
 
 
 	def __exit__(self, type, value, tb):
-		if self.session_type in ("single", "sub"):
-			self.write_lock.unlock()
-			self.write_lock = None
-		else:
-			for lock in self.write_locks:
+		if self.session_type == SessionType.MULTI:
+			for lock in self.write_lock:
 				lock.unlock()
-			self.write_locks = None
-		self.in_session = False
+		else:
+			self.write_lock.unlock()
+		self.write_lock, self.in_session = None, False
 
 
 	def write(self):
 		if not self.in_session:
 			raise PermissionError("Only call write() inside a with statement.")
-
-		if self.session_type == "single":
+		if self.session_type == SessionType.SINGLE:
 			io_unsafe.write(self.db_name, self.data_handle)
-		elif self.session_type == "sub":
+		elif self.session_type == SessionType.SUB:
 			io_unsafe.partial_write(self.partial_handle)
-		else: # Multi
+		elif self.session_type == SessionType.MULTI:
 			for name in self.db_name:
 				io_unsafe.write(name, self.data_handle[name])
