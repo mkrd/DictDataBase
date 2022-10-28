9aea9a849bfc6c05155c60739bf37576729d461d
Marcel Kröker <kroeker.marcel@gmail.com>
2022-10-28 13:35:36 +0200

    wip

--- tests/benchmark/utils.py
+++ tests/benchmark/utils.py
@@ -9,8 +9,8 @@ def make_table(recursion_depth=3, keys_per_level=50):
 	d = {"key1": "val1", "key2": 2, "key3": [1, "2", [3, 3]]}
 	for i in range(recursion_depth):
 		d = {f"key{i}{j}": d for j in range(keys_per_level)}
-	print(f"Made table of size {len(json.dumps(d)) // 1e6}mb")
-	return {"counter": 0, "big": d}
+	#print(f"Made table of size {len(json.dumps(d)) // 1e6}mb")
+	return {"counter": {"counter": 0}, "big": d}
 
 
 def print_stats(i, durations):
@@ -22,27 +22,29 @@ def print_stats(i, durations):
 	# print(f"{i}: total: {len(durations)}, avg: {avg}ms (med: {median}), {min_t}-{max_t}ms")
 
 
-def print_and_assert_results(readers, writers, per_proc, tables, t1, t2):
+def print_and_assert_results(readers, writers, per_proc, tables, big_file, compression, t1, t2):
 	ops = (writers + readers) * per_proc * tables
 	ops_sec = f"{(ops / (t2 - t1)):.0f}"
-	print(f"⏱️ {ops_sec}op/s ({ops} in {t2 - t1:.2f}s), {tables = }, {writers = }")
+	print(f"⏱️ {ops_sec}op/s ({ops} in {t2 - t1:.2f}s), {tables = }, {writers = }, {readers = }, {per_proc = }, {big_file = }, {compression = }")
 	for t in range(tables):
 		db = DDB.at(f"incr{t}").read()
-		assert db["counter"] == per_proc * writers
+		assert db["counter"]["counter"] == per_proc * writers
 		# print(f"✅ counter={db['counter']}")
 
 
 def random_reads(file_count):
 	""" Read the n tables in random order """
 	for t in sorted(range(file_count), key=lambda _: random.random()):
-		DDB.at(f"incr{t}").read()
+		DDB.at(f"incr{t}").read(key="counter")
 
 
 def random_writes(file_count):
 	""" Iterated the n tables in random order and increment the counter """
 	for t in sorted(range(file_count), key=lambda _: random.random()):
-		with DDB.at(f"incr{t}").session(as_type=pd) as (session, d):
-			d["counter"] = lambda x: (x or 0) + 1
+		with DDB.at(f"incr{t}").session(key="counter", as_type=pd) as (session, d):
+
+			d.at("counter").set(d.at("counter").get() + 1)
+
 			session.write()
 
 
