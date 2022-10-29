30f7421a1c1a2c96554d3a2ccaf09db74f6747b3
Marcel Kröker <kroeker.marcel@gmail.com>
2022-10-29 15:06:24 +0200

    do loops

--- profiler.py
+++ profiler.py
@@ -12,7 +12,8 @@ with p:
     # fM44 is small
     # a2lU has many annotations
     # DDB.at("tasks").read(key="fM44", as_type=PathDict)
-    with DDB.at("tasks").session(key="a2lU", as_type=PathDict) as (session, task):
-        task["jay"] = lambda x: (x or 0) + 1
-        session.write()
+    for _ in range(10):
+        with DDB.at("tasks").session(key="a2lU", as_type=PathDict) as (session, task):
+            task["jay"] = lambda x: (x or 0) + 1
+            session.write()
 p.open_in_browser(timeline=True)
