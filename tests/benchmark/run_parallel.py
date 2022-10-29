c2803f5c2f254851f15ab0a801dd27adefec3795
Marcel Kröker <kroeker.marcel@gmail.com>
2022-10-29 12:36:45 +0200

    remove unused code

--- tests/benchmark/run_parallel.py
+++ tests/benchmark/run_parallel.py
@@ -6,7 +6,7 @@ import shutil
 import time
 import os
 
-from utils import incrementor, print_and_assert_results, db_job, make_table
+from utils import print_and_assert_results, db_job, make_table
 
 
 def proc_job(mode, n, tables, sd, uc, uo, id, sk):
@@ -17,33 +17,6 @@ def proc_job(mode, n, tables, sd, uc, uo, id, sk):
 	DDB.config.sort_keys = sk
 	DDB.locking.SLEEP_TIMEOUT = 0.001
 	db_job(mode, tables, n)
-
-
-def parallel_stress(tables=2, proc_count=8, per_process=512):
-	# Create Tables
-	for t in range(tables):
-		DDB.at(f"incr{t}").create({"counter": {"counter": 0}}, force_overwrite=True)
-
-	# Execute process pool running incrementor as the target task
-	t1 = time.monotonic()
-	pool = Pool(processes=proc_count)
-	for i in range(proc_count):
-		# Each process will enter this file again, but not as __main__
-		# So only the outside context is executed, and then the incrementor function
-		# This means we need to pass the config since the process is "fresh"
-		pool.apply_async(proc_job, args=(i, per_process, tables,
-			DDB.config.storage_directory,
-			DDB.config.use_compression,
-			DDB.config.use_orjson,
-			DDB.config.indent,
-			DDB.config.sort_keys,
-		))
-	pool.close()
-	pool.join()
-	t2 = time.monotonic()
-	print_and_assert_results(proc_count, per_process, tables, t1, t2)
-
-
 
 
 def parallel_stressor(file_count, readers, writers, operations_per_process, big_file, compression):
