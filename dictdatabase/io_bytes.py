783ed084255d003b4e3b08e78eb50b71f5f1342f
Marcel Kröker <kroeker.marcel@gmail.com>
2022-11-08 10:52:52 +0100

    wip

--- dictdatabase/io_bytes.py
+++ dictdatabase/io_bytes.py
@@ -15,20 +15,16 @@ def read(db_name: str, start=None, end=None) -> bytes:
 		if ddb_exists:
 			raise FileExistsError(f"DB Inconsistency: \"{db_name}\" exists as .json and .ddb")
 		with open(json_path, "rb") as f:
-			if start is not None:
+			if start is not None and end is not None:
 				f.seek(start)
-				if end is not None:
-					return f.read(end - start)
-				return f.read()
+				return f.read(end - start)
 			return f.read()
 	if not ddb_exists:
 		raise FileNotFoundError(f"DB does not exist: \"{db_name}\"")
 	with open(ddb_path, "rb") as f:
 		json_bytes = zlib.decompress(f.read())
-		if start is not None:
-			if end is not None:
-				return json_bytes[start:end]
-			return json_bytes[start:]
+		if start is not None and end is not None:
+			return json_bytes[start:end]
 		return json_bytes
 
 
@@ -40,19 +36,21 @@ def write(db_name: str, dump: bytes):
 	"""
 	json_path, json_exists, ddb_path, ddb_exists = utils.db_paths(db_name)
 	# Write bytes or string to file
+	remove_this = None
 	if config.use_compression:
 		write_path = ddb_path
 		if json_exists:
-			os.remove(json_path)
+			remove_this = json_path
+		dump = zlib.compress(dump, 1)
 	else:
 		write_path = json_path
 		if ddb_exists:
-			os.remove(ddb_path)
-
-	# Compress if required
-	if config.use_compression:
-		dump = zlib.compress(dump, 1)
+			remove_this = ddb_path
 
 	# Write bytes or string to file
 	with open(write_path, "wb") as f:
 		f.write(dump)
+
+	# Remove the other file if it exists
+	if remove_this is not None:
+		os.remove(remove_this)
