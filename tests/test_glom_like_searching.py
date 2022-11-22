537b800502299b74e3372db54ab48b9fe2aa312e
Danil Tolmachev <danil.tolmachev@alao.ch>
2022-11-22 10:30:04 +0300

    add tests

--- tests/test_glom_like_searching.py
+++ tests/test_glom_like_searching.py
@@ -2,20 +2,27 @@ import dictdatabase as DDB
 
 data = {
     "users": {
-        "Ben": {"age": 30, "job": "Software Engineer"},
+        "Ben": {"age": 30, "job": "Software Engineer", "password": 123},
         "Bob": {"age": 30, "job": "Plumbers"},
     },
-    "Ben": {"job": {"age": 30, "job": "Software Engineer"}},
+    "admin": {"job": {"age": 30, "job": "Software Engineer"}},
 }
 
 
 def test_glom_searching():
     DDB.at("users").create(data, force_overwrite=True)
-    assert DDB.at("users", key="users.Ben.job").read() == 'Software Engineer'
+    assert DDB.at("users", key="users.Ben.job").read() == "Software Engineer"
 
 
 def test_without_glom_searching():
     DDB.at("users").create(data, force_overwrite=True)
     assert DDB.at("users", key="Ben").read() == {
+        "job": {"age": 30, "job": "Software Engineer"}
+    }
+
+
+def test_injection():
+    DDB.at("users").create(data, force_overwrite=True)
+    assert DDB.at("users", key="job").read() == {
         "job": {"age": 30, "job": "Software Engineer"}
     }
