456aef225ca018dc9113b4131ad8d7202a59df65
Danil Tolmachev <danil.tolmachev@alao.ch>
2022-11-24 00:38:11 +0300

    add dataclass for searching

--- dictdatabase/searching.py
+++ dictdatabase/searching.py
@@ -4,9 +4,10 @@ import orjson
 
 from dictdatabase import byte_codes
 from dictdatabase import utils
+from dictdatabase.dataclasses import SearchResult
 
 
-def find_key_position_in_bytes(file: bytes, key: str) -> Tuple[int, int, bool]:
+def find_key_position_in_bytes(file: bytes, key: str) -> SearchResult:
     """
     It finds the start and end indices of the value of a key in a JSON file
 
@@ -19,13 +20,15 @@ def find_key_position_in_bytes(file: bytes, key: str) -> Tuple[int, int, bool]:
     """
     key_start, key_end = utils.find_outermost_key_in_json_bytes(file, key)
     if key_end == -1:
-        return -1, -1, False
+        return SearchResult(start_byte=-1, end_byte=-1, found=False)
     start = key_end + (1 if file[key_end] == byte_codes.SPACE else 0)
     end = utils.seek_index_through_value_bytes(file, start)
-    return start, end, True
+    return SearchResult(start_byte=start, end_byte=end, found=True)
 
 
-def search_key_position_in_db(file: bytes, key: str, glom_searching=True) -> Tuple[int, int, bool]:
+def search_key_position_in_db(
+    file: bytes, key: str, glom_searching=True
+) -> SearchResult:
     original_value_start = 0
     original_value_end = len(file)
     original_key_start = 0
@@ -33,14 +36,14 @@ def search_key_position_in_db(file: bytes, key: str, glom_searching=True) -> Tup
     for k in key.split(".") if glom_searching else [key]:
         key_start, key_end = utils.find_outermost_key_in_json_bytes(file, k)
         if key_end == -1:
-            return -1, -1, False
+            return SearchResult(start_byte=-1, end_byte=-1, found=False)
         original_key_end = original_value_start + key_end
         original_key_start = original_value_start + key_start
-        value_start, value_end, found = find_key_position_in_bytes(file, k)
+        position = find_key_position_in_bytes(file, k)
         original_value_end = original_value_start + original_value_end
-        original_value_start += value_start
+        original_value_start += position.start_byte
         file = file[original_value_start:original_value_end]
-    return original_key_start, original_key_end, True
+    return SearchResult(start_byte=original_key_start, end_byte=original_key_end, found=True)
 
 
 def search_value_position_in_db(
@@ -61,11 +64,11 @@ def search_value_position_in_db(
     original_start = 0
     original_end = len(all_file_bytes)
     for k in key.split(".") if glom_searching else [key]:
-        start, end, found = find_key_position_in_bytes(
+        position = find_key_position_in_bytes(
             all_file_bytes[original_start:original_end], k
         )
-        if not found:
+        if not position.found:
             return -1, -1, False
-        original_end = original_start + end
-        original_start += start
+        original_end = original_start + position.end_byte
+        original_start += position.start_byte
     return original_start, original_end, True
