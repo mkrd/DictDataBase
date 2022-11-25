2864cd116d387d8e0f4176a753db2233f9f99100
Marcel Kröker <kroeker.marcel@gmail.com>
2022-11-25 23:49:54 +0100

    refactor locking

--- dictdatabase/locking.py
+++ dictdatabase/locking.py
@@ -25,27 +25,33 @@ def os_touch(path: str):
 
 class LockFileMeta:
 
-	__slots__ = ("ddb_dir", "name", "id", "time_ns", "stage", "mode")
+	__slots__ = ("ddb_dir", "name", "id", "time_ns", "stage", "mode", "path")
 
 	ddb_dir: str
-	name: str
 	id: str
+	name: str
 	time_ns: str
 	stage: str
 	mode: str
+	path: str
+
+
+	def __repr__(self) -> str:
+		return f"LockFileMeta({self.ddb_dir}, {self.name}, {self.id}, {self.time_ns}, {self.stage}, {self.mode})"
+
 
 	def __init__(self, ddb_dir, name, id, time_ns, stage, mode):
-		self.ddb_dir = ddb_dir
-		self.name = name
-		self.id = id
-		self.time_ns = time_ns
-		self.stage = stage
-		self.mode = mode
+		self.ddb_dir, self.name, self.id = ddb_dir, name, id
+		self.time_ns, self.stage, self.mode = time_ns, stage, mode
+		self._compute_path()
 
-	@property
-	def path(self):
+	def _compute_path(self):
 		lock_file = f"{self.name}.{self.id}.{self.time_ns}.{self.stage}.{self.mode}.lock"
-		return os.path.join(self.ddb_dir, lock_file)
+		self.path = os.path.join(self.ddb_dir, lock_file)
+
+	def set_mode(self, mode):
+		self.mode = mode
+		self._compute_path()
 
 
 class FileLocksSnapshot:
@@ -57,23 +63,21 @@ class FileLocksSnapshot:
 	any_has_write_locks: bool
 	locks: list[LockFileMeta]
 
-	def __init__(self, ddb_dir, db_name, ignore_during_orphan_check):
-		self.any_has_locks = False
-		self.any_write_locks = False
-		self.any_has_write_locks = False
-		self.locks = []
+	def __init__(self, need_lock: LockFileMeta):
+		self.any_has_locks, self.any_write_locks = False, False
+		self.any_has_write_locks, self.locks = False, []
 
-		for x in os.listdir(ddb_dir):
-			if not x.endswith(".lock"):
+		for file_name in os.listdir(need_lock.ddb_dir):
+			if not file_name.endswith(".lock"):
 				continue
-			f_name, f_id, f_time_ns, f_stage, f_mode, _ = x.split(".")
-			if f_name != db_name:
+			name, id, time_ns, stage, mode, _ = file_name.split(".")
+			if name != need_lock.name:
 				continue
 
-			lock_meta = LockFileMeta(ddb_dir, f_name, f_id, f_time_ns, f_stage, f_mode)
+			lock_meta = LockFileMeta(need_lock.ddb_dir, name, id, time_ns, stage, mode)
 
 			# Remove orphaned locks
-			if lock_meta.path != ignore_during_orphan_check:
+			if lock_meta.path != need_lock.path:
 				lock_age = time.monotonic_ns() - int(lock_meta.time_ns)
 				if lock_age > LOCK_TIMEOUT * 1_000_000_000:
 					os.unlink(lock_meta.path)
@@ -90,11 +94,16 @@ class FileLocksSnapshot:
 			if lock_meta.mode == "write":
 				self.any_write_locks = True
 
-	def lock_exists(self, id: str, stage: str, mode: str) -> bool:
-		return any(x.id == id and x.stage == stage and x.mode == mode for x in self.locks)
+	def exists(self, l: LockFileMeta) -> bool:
+		return any(x.id == l.id and x.stage == l.stage and x.mode == l.mode for x in self.locks)
 
-	def get_need_locks(self) -> list[LockFileMeta]:
-		return [l for l in self.locks if l.stage == "need"]
+	def oldest_need(self, need_lock: LockFileMeta) -> bool:
+		# len(need_locks) is at least 1 since this function is only called if there is a need_lock
+		need_locks = [l for l in self.locks if l.stage == "need"]
+		# Sort by time_ns. If multiple, the the one with the smaller id is first
+		need_locks = sorted(need_locks, key=lambda l: int(l.id))
+		need_locks = sorted(need_locks, key=lambda l: int(l.time_ns))
+		return need_locks[0].id == need_lock.id
 
 
 class AbstractLock:
@@ -103,14 +112,10 @@ class AbstractLock:
 		call super().__init__(...) and then only exit __init__ when the lock is aquired.
 	"""
 
-	__slots__ = ("id", "time_ns", "db_name", "need_path", "path", "ddb_dir", "snapshot")
+	__slots__ = ("need_lock", "has_lock", "snapshot")
 
-	id: str
-	time_ns: int
-	db_name: str
-	need_path: str
-	path: str
-	ddb_dir: str
+	need_lock: LockFileMeta
+	has_lock: LockFileMeta
 	snapshot: FileLocksSnapshot
 
 	def __init__(self, db_name: str):
@@ -118,21 +123,25 @@ class AbstractLock:
 			Create a lock, with the current thread id as the lock id,
 			and the current time in nanoseconds as the time.
 		"""
-		self.id = str(threading.get_native_id())
-		self.time_ns = time.monotonic_ns()
-		self.db_name = db_name.replace("/", "___").replace(".", "____")
-		self.ddb_dir = os.path.join(config.storage_directory, ".ddb")
-		if not os.path.isdir(self.ddb_dir):
-			os.mkdir(self.ddb_dir)
+		thread_id = str(threading.get_native_id())
+		time_ns = time.monotonic_ns()
+		name = db_name.replace("/", "___").replace(".", "____")
+		ddb_dir = os.path.join(config.storage_directory, ".ddb")
+
+		self.need_lock = LockFileMeta(ddb_dir, name, thread_id, time_ns, "need", None)
+		self.has_lock = LockFileMeta(ddb_dir, name, thread_id, time_ns, "has", None)
+
+		if not os.path.isdir(ddb_dir):
+			os.mkdir(ddb_dir)
 
 	def _lock(self):
 		raise NotImplementedError
 
 	def _unlock(self):
-		for p in ("need_path", "path"):
+		for p in ("need_lock", "has_lock"):
 			try:
-				if path := getattr(self, p, None):
-					os.unlink(path)
+				if lock := getattr(self, p, None):
+					os.unlink(lock.path)
 			except FileNotFoundError:
 				pass
 			finally:
@@ -144,67 +153,61 @@ class AbstractLock:
 	def __exit__(self, exc_type, exc_val, exc_tb):
 		self._unlock()
 
-	def make_lock_path(self, stage: str, mode: str) -> str:
-		return os.path.join(self.ddb_dir, f"{self.db_name}.{self.id}.{self.time_ns}.{stage}.{mode}.lock")
-
-	def is_oldest_need_lock(self) -> bool:
-		# len(need_locks) is at least 1 since this function is only called if there is a need_lock
-		need_locks = self.snapshot.get_need_locks()
-		# Sort by time_ns. If multiple, the the one with the smaller id is first
-		need_locks = sorted(need_locks, key=lambda l: int(l.id))
-		need_locks = sorted(need_locks, key=lambda l: int(l.time_ns))
-		return need_locks[0].id == self.id
-
 
 class ReadLock(AbstractLock):
+	def __init__(self, db_name: str):
+		super().__init__(db_name)
+		self.need_lock.set_mode("read")
+		self.has_lock.set_mode("read")
+
 	def _lock(self):
 		# Instantly signal that we need to read
-		self.need_path = self.make_lock_path("need", "read")
-		os_touch(self.need_path)
+		os_touch(self.need_lock.path)
 
 		# Except if current thread already has a read lock
-		self.snapshot = FileLocksSnapshot(self.ddb_dir, self.db_name, self.need_path)
-		if self.snapshot.lock_exists(self.id, "has", "read"):
-			os.unlink(self.need_path)
+		self.snapshot = FileLocksSnapshot(self.need_lock)
+		if self.snapshot.exists(self.has_lock):
+			os.unlink(self.need_lock.path)
 			raise RuntimeError("Thread already has a read lock. Do not try to obtain a read lock twice.")
-
-		# Make path of the hyptoetical hasread lock
-		self.path = self.make_lock_path("has", "read")
 
 		# Iterate until this is the oldest need* lock and no haswrite locks exist, or no *write locks exist
 		while True:
 			# If no writing is happening, allow unlimited reading
 			if not self.snapshot.any_write_locks:
-				os_touch(self.path)
-				os.unlink(self.need_path)
+				os_touch(self.has_lock.path)
+				os.unlink(self.need_lock.path)
 				return
 			# A needwrite or haswrite lock exists
-			if not self.snapshot.any_has_write_locks and self.is_oldest_need_lock():
-				os_touch(self.path)
-				os.unlink(self.need_path)
+			if not self.snapshot.any_has_write_locks and self.snapshot.oldest_need(self.need_lock):
+				os_touch(self.has_lock.path)
+				os.unlink(self.need_lock.path)
 				return
 			time.sleep(SLEEP_TIMEOUT)
-			self.snapshot = FileLocksSnapshot(self.ddb_dir, self.db_name, self.need_path)
+			self.snapshot = FileLocksSnapshot(self.need_lock)
 
 
 class WriteLock(AbstractLock):
+	def __init__(self, db_name: str):
+		super().__init__(db_name)
+		self.need_lock.set_mode("write")
+		self.has_lock.set_mode("write")
+
 	def _lock(self):
 		# Instantly signal that we need to write
-		self.path = self.make_lock_path("has", "write")
-		self.need_path = self.make_lock_path("need", "write")
-		os_touch(self.need_path)
+		os_touch(self.need_lock.path)
+
+		self.snapshot = FileLocksSnapshot(self.need_lock)
 
 		# Except if current thread already has a write lock
-		self.snapshot = FileLocksSnapshot(self.ddb_dir, self.db_name, self.need_path)
-		if self.snapshot.lock_exists(self.id, "has", "write"):
-			os.unlink(self.need_path)
+		if self.snapshot.exists(self.has_lock):
+			os.unlink(self.need_lock.path)
 			raise RuntimeError("Thread already has a write lock. Do try to obtain a write lock twice.")
 
 		# Iterate until this is the oldest need* lock and no has* locks exist
 		while True:
-			if not self.snapshot.any_has_locks and self.is_oldest_need_lock():
-				os_touch(self.path)
-				os.unlink(self.need_path)
+			if not self.snapshot.any_has_locks and self.snapshot.oldest_need(self.need_lock):
+				os_touch(self.has_lock.path)
+				os.unlink(self.need_lock.path)
 				return
 			time.sleep(SLEEP_TIMEOUT)
-			self.snapshot = FileLocksSnapshot(self.ddb_dir, self.db_name, self.need_path)
+			self.snapshot = FileLocksSnapshot(self.need_lock)
--- tests/test_locking.py
+++ tests/test_locking.py
@@ -6,23 +6,27 @@ from tests import TEST_DIR
 
 
 def test_double_lock_exception(use_test_dir, use_compression):
+	name = "test_double_lock_exception"
 	with pytest.raises(RuntimeError):
-		with locking.ReadLock("db"):
-			with locking.ReadLock("db"):
+		with locking.ReadLock(name):
+			with locking.ReadLock(name):
 				pass
+
+	ls = locking.FileLocksSnapshot(locking.ReadLock(name).need_lock)
+	assert len(ls.locks) == 0
 
 
 def test_get_lock_names(use_test_dir, use_compression):
 	lock = locking.ReadLock("db")
 	lock._lock()
 
-	ls = locking.FileLocksSnapshot(lock.ddb_dir, "none", lock.need_path)
+	ls = locking.FileLocksSnapshot(locking.ReadLock("none").need_lock)
 	assert ls.locks == []
-	ls = locking.FileLocksSnapshot(lock.ddb_dir, "db", lock.need_path)
+	ls = locking.FileLocksSnapshot(lock.need_lock)
 	assert len(ls.locks) == 1
 
 	assert ls.locks[0].id == str(threading.get_native_id())
-	assert ls.locks[0].time_ns == str(lock.time_ns)
+	assert ls.locks[0].time_ns == str(lock.need_lock.time_ns)
 	assert ls.locks[0].stage == "has"
 	assert ls.locks[0].mode == "read"
 
@@ -39,21 +43,21 @@ def test_get_lock_names(use_test_dir, use_compression):
 def test_remove_orphaned_locks(use_test_dir):
 	prev_config = locking.LOCK_TIMEOUT
 	locking.LOCK_TIMEOUT = 0.1
-	lock = locking.ReadLock("db")
+	lock = locking.ReadLock("test_remove_orphaned_locks")
 	lock._lock()
 
-	ls = locking.FileLocksSnapshot(lock.ddb_dir, "db", lock.need_path)
+	ls = locking.FileLocksSnapshot(lock.need_lock)
 	assert len(ls.locks) == 1
 
 	time.sleep(0.2)
 	# Trigger the removal of orphaned locks
-	ls = locking.FileLocksSnapshot(lock.ddb_dir, "db", lock.need_path)
+	ls = locking.FileLocksSnapshot(lock.need_lock)
 
 	assert len(ls.locks) == 0
 	locking.LOCK_TIMEOUT = prev_config
 
 
 def test_AbstractLock(use_test_dir):
-	l = locking.AbstractLock("test")
+	l = locking.AbstractLock("test_AbstractLock")
 	with pytest.raises(NotImplementedError):
 		l._lock()
