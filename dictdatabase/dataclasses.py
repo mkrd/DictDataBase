b05e6ee4cac7a170c8b95ce7a148e661cdcde3cc
Danil Tolmachev <danil.tolmachev@alao.ch>
2022-11-27 18:00:20 +0300

    add Index dataclass

--- dictdatabase/dataclasses.py
+++ dictdatabase/dataclasses.py
@@ -6,3 +6,14 @@ class SearchResult:
     start_byte: int
     end_byte: int
     found: bool
+
+
+@dataclasses.dataclass(frozen=True)
+class Index:
+    key: str
+    key_start: int
+    key_end: int
+    indent_level: int
+    indent_with: str
+    value_hash: str
+    old_value_end: int
--- dictdatabase/index_manager.py
+++ dictdatabase/index_manager.py
@@ -1,28 +1,27 @@
 import hashlib
 
 from dictdatabase import utils
+from dictdatabase.dataclasses import Index
 
 
-class IndexManager:
-    @staticmethod
-    def create_index(all_file_bytes: bytes, key: str, start, end):
-        """
-        It takes a JSON file, a key, and a start and end position, and returns a tuple of information about the key and its
-        value
+def create_index(all_file_bytes: bytes, key: str, start, end) -> Index:
+    """
+    It takes a JSON file, a key, and a start and end position, and returns a tuple of information about the key and its
+    value
 
-        Args:
-          all_file_bytes (bytes): The entire file as a byte string.
-          key (str): The key of the value we're indexing.
-          start: the start of the value in the file
-          end: the end of the value in the file
+    Args:
+      all_file_bytes (bytes): The entire file as a byte string.
+      key (str): The key of the value we're indexing.
+      start: the start of the value in the file
+      end: the end of the value in the file
 
-        Returns:
-          The key, start, end, indent_level, indent_with, value_hash, end
-        """
-        key_start, key_end = utils.find_outermost_key_in_json_bytes(all_file_bytes, key)
-        indent_level, indent_with = utils.detect_indentation_in_json_bytes(
-            all_file_bytes, key_start
-        )
-        value_bytes = all_file_bytes[start:end]
-        value_hash = hashlib.sha256(value_bytes).hexdigest()
-        return key, start, end, indent_level, indent_with, value_hash, end
+    Returns:
+      The key, start, end, indent_level, indent_with, value_hash, end
+    """
+    key_start, key_end = utils.find_outermost_key_in_json_bytes(all_file_bytes, key)
+    indent_level, indent_with = utils.detect_indentation_in_json_bytes(
+        all_file_bytes, key_start
+    )
+    value_bytes = all_file_bytes[start:end]
+    value_hash = hashlib.sha256(value_bytes).hexdigest()
+    return Index(key, start, end, indent_level, indent_with, value_hash, end)
