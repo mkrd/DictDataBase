25756431c4199528f8340b1a1d75bc267a749b0a
Marcel Kröker <kroeker.marcel@gmail.com>
2023-08-13 15:59:13 +0200

    add pre checks

---
+++ test_tick_rate.py
@@ -0,0 +1,30 @@
+import time
+
+def get_tick_rate(clock_func, duration=1.0):
+    start_time = time.time()
+    end_time = start_time + duration
+
+    # Initial reading
+    prev_value = clock_func()
+    ticks = 0
+
+    while time.time() < end_time:
+        current_value = clock_func()
+        if current_value != prev_value:
+            ticks += 1
+        prev_value = current_value
+
+    return ticks / duration  # ticks per second
+
+if __name__ == "__main__":
+    clock_funcs = {
+        "time           ": time.time,
+        "time_ns        ": time.time_ns,
+        "monotonic      ": time.monotonic,
+        "monotonic_ns   ": time.monotonic_ns,
+        "perf_counter   ": time.perf_counter,
+        "perf_counter_ns": time.perf_counter_ns,
+    }
+
+    for name, func in clock_funcs.items():
+        print(f"Tick rate for {name}: {get_tick_rate(func)} ticks/second")
