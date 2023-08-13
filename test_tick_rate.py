3c13fba0e4456c0d7b013add771aad044c4d0673
Marcel Kröker <kroeker.marcel@gmail.com>
2023-08-13 16:11:07 +0200

    error on non-monotonic

--- test_tick_rate.py
+++ test_tick_rate.py
@@ -11,6 +11,8 @@ def get_tick_rate(clock_func, duration=1.0):
 
     while time.time() < end_time:
         current_value = clock_func()
+        if current_value < prev_value:
+            raise RuntimeError("Clock function is not monotonic")
         if current_value != prev_value:
             ticks += 1
         prev_value = current_value
